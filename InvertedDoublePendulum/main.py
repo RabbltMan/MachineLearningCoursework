5758205697cd55d34a14429d1d6837a6747d026b
RabbltMan <82202653+RabbltMan@users.noreply.github.com>
2023-11-09 23:05:51 +0800

    add ppo implementation

--- InvertedDoublePendulum/main.py
+++ InvertedDoublePendulum/main.py
@@ -1,5 +1,8 @@
-import torch
+from matplotlib import pyplot as plt
+import numpy as np
 from gymnasium import make
+
+import networks.continuous_ppo as ppo
 
 
 class InvertedDoublePendulum:
@@ -12,7 +15,59 @@ class InvertedDoublePendulum:
         self.hidden_cells = 128
         self.env = make("InvertedDoublePendulum-v4", render_mode="human")
         num_actions = self.env.action_space.shape[0]
+        policy_lr = 0.005
+        value_lr = 0.005
+        lambda_ = 0.95
+        epochs = 100
+        epsilon = 0.2
+        gamma = 0.9
+        self.agent = ppo.Model(hidden_cells=self.hidden_cells,
+                               actions=num_actions,
+                               policy_lr=policy_lr,
+                               value_lr=value_lr,
+                               lambda_=lambda_,
+                               epochs=epochs,
+                               epsilon=epsilon,
+                               gamma=gamma)
+        return_list = []
+        for i in range(5_000):
+            state = self.env.reset()[0]  # 环境重置
+            done = False  # 任务完成的标记
+            episode_return = 0  # 累计每回合的reward
 
+            # 构造数据集，保存每个回合的状态数据
+            transition_dict = {
+                'states': [],
+                'actions': [],
+                'next_states': [],
+                'rewards': [],
+                'dones': [],
+            }
+
+            while not done:
+                action = self.agent.next_action(state)  # 动作选择
+                next_state, reward, done, _, _ = self.env.step(action)  # 环境更新
+                transition_dict['states'].append(state)
+                transition_dict['actions'].append(action)
+                transition_dict['next_states'].append(next_state)
+                transition_dict['rewards'].append(reward)
+                transition_dict['dones'].append(done)
+                # 更新状态
+                state = next_state
+                # 累计回合奖励
+                episode_return += reward
+
+            # 保存每个回合的return
+            return_list.append(episode_return)
+            # 模型训练
+            self.agent.update(transition_dict)
+
+            # 打印回合信息
+            print(f'iter:{i}, return:{np.mean(return_list[-10:])}')
+
+        plt.plot(return_list)
+        plt.title('return')
+        plt.show()
 
 
 InvertedDoublePendulum()
--- InvertedDoublePendulum/networks/continuous_ppo.py
+++ InvertedDoublePendulum/networks/continuous_ppo.py
@@ -2,8 +2,6 @@ import numpy as np
 import torch
 from torch import cuda, distributions, nn, optim
 
-import networks.continuous_ppo as ppo
-
 
 class PolicyNet(nn.Module):
     """
@@ -25,6 +23,7 @@ class PolicyNet(nn.Module):
         """
         The forward propagation of `PPO Policy Network`
         """
+        x = x.to(self.linear_1.weight.dtype)
         x = self.linear_1(x)
         x = self.relu(x)
         mu = self.linear_2(x)
@@ -51,6 +50,7 @@ class ValueNet(nn.Module):
         """
         The forward propagation of `PPO Value Network`
         """
+        x = x.to(self.linear_1.weight.dtype)
         x = self.linear_1(x)
         x = self.relu(x)
         x = self.linear_2(x)
@@ -65,10 +65,10 @@ class Model:
     def __init__(self, hidden_cells: int, actions: int, policy_lr: float,
                  value_lr: float, lambda_: float, epochs: int, epsilon: float,
                  gamma: float) -> None:
-        self.policy_net = ppo.PolicyNet(hidden_cells, actions)
-        self.value_net = ppo.ValueNet(hidden_cells)
+        self.policy_net = PolicyNet(hidden_cells, actions)
+        self.value_net = ValueNet(hidden_cells)
         self.policy_optim = optim.Adam(self.policy_net.parameters(), policy_lr)
-        self.value_optim = optim.Adam(self.value_net, value_lr)
+        self.value_optim = optim.Adam(self.value_net.parameters(), value_lr)
         self.lambda_ = lambda_
         self.epochs = epochs
         self.epsilon = epsilon
@@ -86,12 +86,87 @@ class Model:
         if cuda.is_available():
             current_state = current_state.cuda()
         mu, std = self.policy_net(current_state)
-        action_dict = distributions.Normal(mu, std)
-        action = list(action_dict.sample().item())
+        try:
+            action_dict = distributions.Normal(mu, std)
+        except ValueError:
+            action_dict = distributions.Normal(torch.tensor([[0.1]]),
+                                               torch.tensor([[0.1]]))
+        action = [action_dict.sample().item()]
         return action
 
-    def update(self, transtition_dict):
+    def update(self, transition_dict):
         """
         A method which trains and updates policy and value networks.
         """
-        pass
+        states = torch.tensor(transition_dict['states'], dtype=torch.float)
+        actions = torch.tensor(transition_dict['actions'],
+                               dtype=torch.float).view(-1, 1)
+        rewards = torch.tensor(transition_dict['rewards'],
+                               dtype=torch.float).view(-1, 1)
+        next_states = torch.tensor(transition_dict['next_states'],
+                                   dtype=torch.float)
+        dones = torch.tensor(transition_dict['dones'],
+                             dtype=torch.float).view(-1, 1)
+        if cuda.is_available():
+            states = states.cuda()
+            actions = actions.cuda()
+            rewards = rewards.cuda()
+            next_states = next_states.cuda()
+            dones = dones.cuda()
+
+        # [b,states] -> [b,1]
+        next_states_target = self.value_net(next_states)
+        # temporal difference target，which computes state_value of shape [b,1] at current tick
+        td_target = rewards + self.gamma * next_states_target * (1 - dones)
+        # temporal difference prediction，which computes state_value of shape [b,1]
+        td_value = self.value_net(states)
+        # temporal difference result [b,1]
+        td_delta = td_value - td_target
+
+        # compute GAE advantage
+        td_delta = td_delta.cpu().detach().numpy()  # [b,1]
+        advantage_list = []
+        advantage = 0
+        for delta in td_delta[::-1]:
+            advantage = self.gamma * self.lambda_ * advantage + delta
+            advantage_list.append(advantage)
+        advantage_list.reverse()
+        advantage = torch.tensor(advantage_list, dtype=torch.float)
+
+        # predict Gaussian Distribution params of current state action
+        mu, std = self.policy_net(states)  # [b,1]
+        try:
+            action_dists = torch.distributions.Normal(mu.detach(), std.detach())
+        except ValueError:
+            action_dists = distributions.Normal(torch.tensor([[0.1]]).cuda(),
+                                               torch.tensor([[0.1]]).cuda())
+        # decide action with log probability func from normal distribution
+        old_log_prob = action_dists.log_prob(actions)
+
+        # train with current seq
+        for _ in range(self.epochs):
+            # predict Gaussian Distribution params of current state action
+            mu, std = self.policy_net(states)
+            try:
+                action_dists = torch.distributions.Normal(mu, std)
+            except ValueError:
+                continue
+            # log probability of action Agent takes at tick t in state s under current policy
+            log_prob = action_dists.log_prob(actions)
+            # ratio for policy update
+            ratio = torch.exp(log_prob - old_log_prob).cpu()
+
+            # PPO Clip loss
+            surr1 = ratio * advantage
+            surr2 = torch.clamp(ratio, 1 - self.epsilon, 1 + self.epsilon)
+            value_net_loss = torch.mean(-torch.min(surr1, surr2))
+            policy_net_loss = torch.mean(
+                nn.functional.mse_loss(self.value_net(states),
+                                       td_target.detach()))
+            # backward propagation
+            self.policy_optim.zero_grad()
+            self.value_optim.zero_grad()
+            policy_net_loss.backward()
+            value_net_loss.backward()
+            self.policy_optim.step()
+            self.value_optim.step()
